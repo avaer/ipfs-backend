1257160bf590656d30621828fa92a6ee92758530
Muhammad Abeer <abeersaqib948@gmail.com>
2022-03-26 17:10:58 +0500

    Implemented IPFS Head and Reworked docs

--- constants.js
+++ constants.js
@@ -1,9 +1,21 @@
 const MAX_SIZE = 100 * 1024 * 1024;
 const IPFS_HTTP_PORT = 8080;
 const IPFS_PORT = 5001;
+const ALLOWED_METHODS = [
+  'GET',
+  'HEAD'
+]
+
+const ALLOWED_HOSTS = [
+  'ipfs.webaverse.com',
+  'ipfs.exokit.org',
+  'local.webaverse.com'
+]
 
 module.exports = {
   MAX_SIZE,
   IPFS_HTTP_PORT,
   IPFS_PORT,
+  ALLOWED_METHODS,
+  ALLOWED_HOSTS
 };
